874d8e2f52477ee62a55b1486ae2abcfee92e343
Abhikansh Mittal <abhikansh33@gmail.com>
2025-10-15 13:17:58 +0530

    backend

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({port:8080});
+
+let userCount = 0;
+
+
+wss.on('connection', (socket)=>{
+    console.log('user connect')
+    userCount++
+    console.log('userConnect#'+userCount);
+
+    socket.on('message', (message) => { 
+        console.log( "message recieved" , message.toString());
+        setTimeout(()=>{
+        socket.send(message.toString() + ":sent from server")
+        },1000)
+    })
+})
+
+
