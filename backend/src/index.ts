f152cdd43d7432320561996ed212116762d7cfac
Abhikansh Mittal <abhikansh33@gmail.com>
2025-10-15 22:27:24 +0530

    added logic for room based chat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,71 @@
+//chat app
+// import { WebSocketServer , WebSocket} from "ws";
+// const wss = new WebSocketServer({port:8080});
 
-import { WebSocketServer } from "ws";
+// let userCount = 0;
+// let allSockets:WebSocket[] = []
+
+// wss.on('connection', (socket)=>{
+//     allSockets.push(socket)
+//     console.log('user connect')
+//     userCount++
+//     console.log('userConnect#'+userCount);
+
+//     socket.on('message', (message) => { 
+//         console.log( "message recieved" , message.toString());
+//         // setTimeout(()=>{
+//         // socket.send(message.toString() + ":sent from server")
+//         // },1000)
+//         allSockets.forEach(s=>{
+//             s.send(message.toString()+": sent from server");
+//         })
+//     })
+
+//     socket.on('disconnect',()=>{
+//         allSockets = allSockets.filter(x => x != socket)
+//     })
+// })
+
+//room logic
+import { WebSocketServer , WebSocket} from "ws";
 const wss = new WebSocketServer({port:8080});
-
-let userCount = 0;
-
+interface User {
+    socket:WebSocket;
+    room:string;
+}
+let allSockets:User[] = []
 
 wss.on('connection', (socket)=>{
+    allSockets.push(socket)
     console.log('user connect')
-    userCount++
-    console.log('userConnect#'+userCount);
 
     socket.on('message', (message) => { 
-        console.log( "message recieved" , message.toString());
-        setTimeout(()=>{
-        socket.send(message.toString() + ":sent from server")
-        },1000)
+     const parsedMessage = JSON.parse(message);
+     if(parsedMessage.type === 'join'){
+        allSockets.push({
+            socket,
+            room:parsedMessage.payload.roomId
+        })
+     }
+     if(parsedMessage.type === 'chat'){
+        // const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room
+        let currentUserRoom = null;
+        for(let i = 0; i<allSockets.length;i++){
+            if(allSockets[i]?.socket == socket){
+              currentUserRoom =   allSockets[i]?.room 
+            }
+        }
+        for(let i =0;i<allSockets.length;i++){
+            if(allSockets[i]?.room == currentUserRoom){
+                allSockets[i]?.socket.send(parsedMessage.payload.message)
+            }
+        }
+        
+     }
+    })
+
+    socket.on('disconnect',()=>{
+        allSockets = allSockets.filter(x => x != socket)
     })
 })
-
 
